dd605c794365adbab9b5e08100488308f1741501
Eugenia <eugenia.cebotarenco@isa.utm.md>
2025-03-12 21:00:27 +0200

    Am adaugat@Html.Partial pe toate paginile , modificari la controller

--- SkillSwaps/Controllers/BlogController.cs
+++ SkillSwaps/Controllers/BlogController.cs
@@ -13,5 +13,93 @@ namespace SkillSwaps.Controllers
         {
             return View();
         }
+
+        public ActionResult Ev1()
+        {
+            return View();
+        }
+
+        public ActionResult Ev2()
+        {
+            return View();
+        }
+
+        public ActionResult Ev3()
+        {
+            return View();
+        }
+
+        public ActionResult Ev4()
+        {
+            return View();
+        }
+
+        public ActionResult Ev5()
+        {
+            return View();
+        }
+
+        public ActionResult Ev6()
+        {
+            return View();
+        }
+
+        public ActionResult Ev7()
+        {
+            return View();
+        }
+
+        public ActionResult Ev8()
+        {
+            return View();
+        }
+
+        public ActionResult Ev9()
+        {
+            return View();
+        }
+
+        public ActionResult Ev10()
+        {
+            return View();
+        }
+
+        public ActionResult Ev11()
+        {
+            return View();
+        }
+
+        public ActionResult Ev12()
+        {
+            return View();
+        }
+
+        public ActionResult Ev13()
+        {
+            return View();
+        }
+
+        public ActionResult Ev14()
+        {
+            return View();
+        }
+
+        public ActionResult Ev15()
+        {
+            return View();
+        }
+
+        public ActionResult Evpag2()
+        {
+            return View();
+        }
+
+        public ActionResult Evpag3()
+        {
+            return View();
+        }
+
     }
+
+
 }
