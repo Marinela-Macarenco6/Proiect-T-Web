a57087247264f0ef8ed1da6ad7769de5c40e6201
Eugenia <eugenia.cebotarenco@isa.utm.md>
2025-03-12 12:13:48 +0200

    Modificarea paginii blog, adaugarea evenimente

---
+++ SkillSwaps/Controllers/Ev10Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SkillSwaps.Controllers
+{
+    public class Ev10Controller : Controller
+    {
+        // GET: Ev10
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SkillSwaps/Controllers/Ev6Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SkillSwaps.Controllers
+{
+    public class Ev6Controller : Controller
+    {
+        // GET: Ev6
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
