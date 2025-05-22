918682b02f3a990d04cfa17b9a7c89019e728642
Marinela-Macarenco <marinelamacarenco48@gmail.com>
2025-05-22 10:24:44 +0300

    Adăugarea coșului , stilizarea acestuia .Adăugarea funcționalului - adaugă în coș , șterge din coș .

--- SkillSwaps/Controllers/CartController.cs
+++ SkillSwaps/Controllers/CartController.cs
@@ -1,17 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using SkillSwaps.Models.Courses;
 
 namespace SkillSwaps.Controllers
 {
     public class CartController : Controller
     {
-        // GET: Cart
         public ActionResult Index()
         {
-            return View();
+            var sessionCart = Session["Cart"] as string;
+            var cart = sessionCart != null
+                ? JsonConvert.DeserializeObject<List<CartItem>>(sessionCart)
+                : new List<CartItem>();
+
+            return View(cart);
         }
+
+        [HttpPost]
+        public ActionResult AddToCart(int courseId, string courseName, decimal price)
+        {
+            var sessionCart = Session["Cart"] as string;
+            var cart = sessionCart != null
+                ? JsonConvert.DeserializeObject<List<CartItem>>(sessionCart)
+                : new List<CartItem>();
+
+            cart.Add(new CartItem
+            {
+                CourseId = courseId,
+                CourseName = courseName,
+                Price = price
+            });
+
+            Session["Cart"] = JsonConvert.SerializeObject(cart);
+
+            return RedirectToAction("Index", "Cart");
+        }
+
+        [HttpPost]
+        public ActionResult RemoveFromCart(int courseId)
+        {
+            var sessionCart = Session["Cart"] as string;
+            if (sessionCart != null)
+            {
+                var cart = JsonConvert.DeserializeObject<List<CartItem>>(sessionCart);
+                var itemToRemove = cart.FirstOrDefault(c => c.CourseId == courseId);
+                if (itemToRemove != null)
+                {
+                    cart.Remove(itemToRemove);
+                    Session["Cart"] = JsonConvert.SerializeObject(cart);
+                }
+            }
+
+            return RedirectToAction("Index", "Cart");
+        }
+
     }
 }
---
+++ SkillSwaps/Models/Course/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SkillSwaps.Models.Courses
+{
+    public class CartItem
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
